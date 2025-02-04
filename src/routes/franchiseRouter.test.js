7cef68d7fe446cc84c6f0349dd46a83d35675674
davidsumsion <davidsumsion20@gmail.com>
2025-02-04 12:25:24 -0700

    chore: franchise router finished"

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -5,6 +5,7 @@ const db = require('../database/database')
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 const defaultAdmin = { email: 'a@jwt.com', password: 'admin', name: '常用名字' };
 let defaultAdminAuthToken;
+let testUserId;
 
 if (process.env.VSCODE_INSPECTOR_OPTIONS) {
     jest.setTimeout(60 * 1000 * 5); // 5 minutes
@@ -29,18 +30,78 @@ test('make a franchises while not Admin', async () => {
 });
 
 test('make a franchise while Admin', async () => {
-    //create a user
-    testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    const registerRes = await request(app).post('/api/auth').send(testUser);
-    testUserAuthToken = registerRes.body.token;
-    testFranchise.admins.push({email: testUser.email});
-    testFranchise.name = Math.random().toString(36).substring(2, 12)
-    const makeReq = await request(app).post('/api/franchise').set('Authorization', `Bearer ${defaultAdminAuthToken}`).send(testFranchise);
+    const email = Math.random().toString(36).substring(2, 12) + '@test.com'
+    const makeReq = await createFranchise(email);
     expect(makeReq.status).toBe(200);
     expect(makeReq.body.admins[0].email).toEqual(testUser.email);
     expect(makeReq.body.admins[0].name).toEqual(testUser.name);
 })
 
 test('create a store', async () => {
-    
+    const email = Math.random().toString(36).substring(2, 12) + '@test.com'
+    const makeReq = await createFranchise(email);
+    expect(makeReq.status).toBe(200);
+    const newStoreReq = await createStore(makeReq);
+    expect(newStoreReq.status).toBe(200);
+    expect(newStoreReq.body.franchiseId).toEqual(makeReq.body.id);
 });
+
+test('delete a store', async () => {
+    const email = Math.random().toString(36).substring(2, 12) + '@test.com'
+    const makeReq = await createFranchise(email);
+    expect(makeReq.status).toBe(200);
+    const newStoreReq = await createStore(makeReq);
+    expect(newStoreReq.status).toBe(200);
+    const deleteStoreReq = await request(app).delete('/api/franchise/' + makeReq.body.id + '/store/' + newStoreReq.body.franchiseId).set('Authorization', `Bearer ${defaultAdminAuthToken}`)
+    expect(deleteStoreReq.body.message).toEqual('store deleted');
+})
+
+// test('delete a store that doesn\'t exist', async () => {
+//     const makeReq = await createFranchise();
+//     expect(makeReq.status).toBe(200);
+//     const newStoreReq = await createStore(makeReq);
+//     expect(newStoreReq.status).toBe(200);
+//     const deleteStoreReq = await request(app).delete('/api/franchise/' + "oneTwoThree" + '/store/' + 50000).set('Authorization', `Bearer ${defaultAdminAuthToken}`)
+//     expect(deleteStoreReq.body.message).toEqual('store deleted');
+// })
+
+test('delete a franchise', async () => {
+    const email = Math.random().toString(36).substring(2, 12) + '@test.com'
+    const makeReq = await createFranchise(email);
+    expect(makeReq.status).toBe(200);
+    const deleteFranchiseReq = await request(app).delete('/api/franchise/' + makeReq.body.id).set('Authorization', `Bearer ${defaultAdminAuthToken}`)
+    expect(deleteFranchiseReq.body.message).toEqual('franchise deleted');
+})
+
+test('get user franchises', async () => {
+    const email = Math.random().toString(36).substring(2, 12) + '@test.com'
+    testUser.email = email;
+
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+    expect(registerRes.status).toBe(200);
+
+    const userId = registerRes.body.user.id;
+    const testFranchise1 = { "stores": [], "id": "", "name": Math.random().toString(36).substring(2, 12), "admins": [{email: email}] }
+    const testFranchise2 = { "stores": [], "id": "", "name": Math.random().toString(36).substring(2, 12), "admins": [{email: email}] }
+    const fran1Req = await request(app).post('/api/franchise').set('Authorization', `Bearer ${defaultAdminAuthToken}`).send(testFranchise1);
+    const fran2Req = await request(app).post('/api/franchise').set('Authorization', `Bearer ${defaultAdminAuthToken}`).send(testFranchise2);
+    expect(fran1Req.status).toBe(200);
+    expect(fran2Req.status).toBe(200);
+
+    const getUserFranchiseReq = await request(app).get('/api/franchise/' + userId).set('Authorization', `Bearer ${defaultAdminAuthToken}`);
+    expect(getUserFranchiseReq.body.length).toBe(2);
+})
+
+async function createFranchise(email) {
+    testUser.email = email;
+    const registerRes = await request(app).post('/api/auth').send(testUser);
+    testUserAuthToken = registerRes.body.token;
+    testFranchise.admins.push({email: testUser.email});
+    testFranchise.name = Math.random().toString(36).substring(2, 12)
+    return await request(app).post('/api/franchise').set('Authorization', `Bearer ${defaultAdminAuthToken}`).send(testFranchise);
+}
+
+async function createStore(makeReq) {
+    const newStore = {"franchiseId": makeReq.body.id, "name":Math.random().toString(36).substring(2, 12)}
+    return await request(app).post('/api/franchise/' + makeReq.body.id + '/store').set('Authorization', `Bearer ${defaultAdminAuthToken}`).send(newStore);
+}
